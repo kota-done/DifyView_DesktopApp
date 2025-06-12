22107b6386694f7969772f9c48e7574499885735
kota-done <shinigami0706@gmail.com>
2025-06-12 19:12:09 +0900

    クラス名確定により、テストファイルの名称変更

---
+++ src/test/AppSettingDtoTest.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package test;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * 
+ */
+public class AppSettingDtoTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		 // パターン①：正常処理　インスタンス生成＋生成済みインスタンス取得
+		
+		// パターン②：
+		// 
+		// 
+		// 
+	}
+
+}

a82e627f1224bb188a8dfa185c90f0500684c1d3
kota-done <shinigami0706@gmail.com>
2025-06-22 07:11:39 +0900

    設定ファイル読み込み処理のUT完了。

--- src/app/util/CommonFunction.java
+++ src/app/util/CommonFunction.java
@@ -3,6 +3,10 @@
  */
 package app.util;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  * 共通関数クラス
  * @author 
@@ -13,6 +17,7 @@ public class CommonFunction {
 	/************
 	* メソッド名：Nullおよび空白チェック処理
 	* 処理内容：可変長引数の中身をチェック。
+	* @param checkStrings チェックしたいString型の変数（複数可能）
 	************/
 	public static void checkNullBlank(String ...checkStrings) {
 		int checkNum = checkStrings.length;
@@ -30,6 +35,24 @@ public class CommonFunction {
 		}
 		if(errorMessage.length()>0) {
 			throw new IllegalArgumentException("入力チェックエラー: " + errorMessage.toString());
+		}
+	}
+	
+    /**
+     * 設定ファイルを読み込む（プロパティ読み込みのみ）
+     * @param filePath 設定ファイルのパス
+     * @return 
+     * @throws IOException 読み込み失敗時
+     */
+	public static Properties load(String proFile) throws IOException {
+		Properties props = new Properties();
+//		try (InputStream proIn = CommonFunction.class.getClass().getClassLoader().getResourceAsStream(proFile)) {
+			try (InputStream proIn = CommonFunction.class.getResourceAsStream(proFile)) {
+			if(proIn == null) {
+				throw new IllegalArgumentException(proFile+":読み込みファイルが見つかりません。");
+			}
+			props.load(proIn);
+			return props;
 		}
 	}
 }
